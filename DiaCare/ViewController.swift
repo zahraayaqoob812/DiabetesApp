f8de928f0c3ad6b05769e11e0aa55da4492d1984
zahraayaqoob812 <zahraayaqoob1998@gmail.com>
2022-12-04 10:07:28 +0300

    remove

--- DiaCare/ViewController.swift
+++ DiaCare/ViewController.swift
@@ -12,11 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print(reverse(text: "stressed"))
     }
 
-    func reverse(text: String) -> String {
-        return String(text.reversed())
-    }
 }
 
