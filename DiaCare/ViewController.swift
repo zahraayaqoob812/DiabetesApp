95b1d4fdf16ce390745fc609894258246f8fe3ad
zahraayaqoob812 <zahraayaqoob1998@gmail.com>
2022-12-04 10:04:29 +0300

    function

--- DiaCare/ViewController.swift
+++ DiaCare/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(reverse(text: "stressed"))
     }
 
-
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
 }
 
