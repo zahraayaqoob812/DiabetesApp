e2e7ffe5b29477173a920a38ea651324764e0afd
zahraayaqoob812 <zahraayaqoob1998@gmail.com>
2022-12-04 10:19:22 +0300

    view controller change

--- DiaCare/ViewController.swift
+++ DiaCare/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print(reverse(text: "stressed"))
+        print(reverse(text: "Zahra"))
     }
     func reverse(text: String) -> String {
         return String(text.reversed())
